6ec2b3d85d0ad1d847e49b145d37d73bfe400f37
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-06 20:09:06 +0800

    build: create a reference of the tweet btn

--- javascript/script.js
+++ javascript/script.js
@@ -2,12 +2,17 @@ const apiUrl = "https://api.gameofthronesquotes.xyz/v1/random";
 const quote = document.querySelector("#quote");
 const author = document.querySelector("#author")
 const newBtn = document.querySelector("#new-btn");
+const tweetBtn = document.querySelector("#tweet-btn");
 
 async function getQuote () {
   const response = await fetch(apiUrl);
   let data = await response.json();
   quote.textContent = data.sentence;
   author.textContent = data.character.name;
+}
+
+function tweet() {
+  window.open("https://x.com/intent/tweet?text=Hello%20world")
 }
 
 getQuote();
