9240aefdc8d8e1cb91131a0f4c721b9e11a4f4cd
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-06 20:03:47 +0800

    fix: implement the url inside the func

--- javascript/script.js
+++ javascript/script.js
@@ -3,9 +3,12 @@ const quote = document.querySelector("#quote");
 const author = document.querySelector("#author")
 const newBtn = document.querySelector("#new-btn");
 
-async function getQuote (url) {
-  const response = await fetch(url);
+async function getQuote () {
+  const response = await fetch(apiUrl);
   let data = await response.json();
   quote.textContent = data.sentence;
   author.textContent = data.character.name;
 }
+
+
+
